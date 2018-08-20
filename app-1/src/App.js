2b1e47980885d50f0f4dabe7ff01c15093a30182
Brady Snuggs <dronebug360@gmail.com>
2018-08-19 19:48:46 -1000

    changing stuffs

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -1,24 +1,23 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react';
 import './App.css'
 
-export default class App extends Component{
+export default class App extends Component {
   constructor(props){
     super(props)
-    this.state={
-      userMessage: ''
+    this.state = {
+      message: ''
     }
-  }
+}
 
-handlemessageChange(value){
-  this.setState({userMessage: value})
+handleChange(value){
+  this.setState({message: value})
 }
 
 render(){
   return(
     <div className="App">
-    <h1>Watch Magic Happen as You Type</h1>
-    <input onChange={(e)=>this.handlemessageChange(e.target.value)} />
-    <h1 className="headerColor">{this.state.userMessage}</h1>
+    <input onChange={((e)=> this.handleChange(e.target.value))}/>
+    <h1> {this.state.message} </h1>
     </div>
   )
 }
